05e0683c36c8119e2f71f9b82acf96d60ad0daae
Bhavya Sharma1 <bhavya.sharma1@globallogic.com>
2022-02-21 23:47:05 +0530

    Initial commit

---
+++ app/src/main/java/com/rajdev/loanit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rajdev.loanit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
